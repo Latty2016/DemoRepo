a16f48e1232bd13378cabe23a7d5a4b60e7e71ce
Latif Altay <latif0706@gmail.com>
2024-01-02 09:19:32 -0500

    one method added to the homePage
    homePage1() added

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -5,4 +5,7 @@ public class HomePage {
     public void homePage(){
         System.out.println("Homepage...");
     }
+    public void homePage1(){
+           System.out.println("Homepage1...");
+    }
 }
